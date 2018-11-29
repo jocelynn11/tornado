2811c361516a3dc369c59838e218353480a1c1f2
jocelynn11 <kyriepham2@gmail.com>
2018-11-29 21:12:05 +0700

    btvn

---
+++ Part1.3.py
@@ -0,0 +1,29 @@
+from turtle import*
+shape("turtle")
+color("green")
+for i in range(30):
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    right(5)
+    forward(100)
+    left(90)
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+mainloop()
